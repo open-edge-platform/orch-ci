16ba2eca8e6277f594c7d96251ed6c4f90c342d0
Ciprian Goea <florin.c.goea@intel.com>
2025-04-04 12:22:26 +0300

    fix linter

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,3 +1,6 @@
+// SPDX-FileCopyrightText: (C) 2025 Intel Corporation
+// SPDX-License-Identifier: Apache-2.0
+
 // commitlint.config.js
 module.exports = {
     extends: ['@commitlint/config-conventional'],
