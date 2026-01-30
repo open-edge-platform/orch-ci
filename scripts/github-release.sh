1d9fde1e64081a425f622e12dd7737fc93371bfa
manilk1x <anil1x.kumar.m@intel.com>
2026-01-30 10:51:51 +0530

    Copyright updated to 2026

--- scripts/github-release.sh
+++ scripts/github-release.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-# SPDX-FileCopyrightText: (C) 2025 Intel Corporation
+# SPDX-FileCopyrightText: (C) 2026 Intel Corporation
 # SPDX-License-Identifier: Apache-2.0
 
 # halt script if an error occurs
